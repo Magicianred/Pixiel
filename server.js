dc076ed5291398efe7d3dee16564fb4e157a863a
Choppychan <s.carmen2101@gmail.com>
2020-07-06 20:00:21 +0200

    Creato un piccolo web server

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
+//requisito minimo di ogni web app scritta in Node
 var http = require('http');
-var url = require('url');
 
 var server = http.createServer(function(req, res) {
-  var page = url.parse(req.url).pathname;
-  console.log(page);
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  if (page == '/') {
-    res.write('Sei nella Reception. Posso aiutarti?');
-  } else if (page == '/basement') {
-    res.write('Sei nella cantina dei vini. Le bottiglie sono mie!');
-  }  else if (page == '/floor/1/bedroom') {
-    res.write('Oh, non dovresti essere qui!');
-  } else if (404, {"Content-Type" : "text/plain"}) {
-  res.write('Ci sei cascato!');
-}
-  res.end();
+  res.writeHead(200);
+  res.end('Ciao a tutti, sono un web server!');
 });
 server.listen(8080);
