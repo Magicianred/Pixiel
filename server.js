3a9dc56c356910b8643dca267c08333e6a57fc09
Choppychan <s.carmen2101@gmail.com>
2020-07-06 20:31:32 +0200

    Aggiunti commenti

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //requisito minimo di ogni web app scritta in Node
 var http = require('http');
 
+//La variabile http rappresenta l’oggetto JavaScript che ci permetterà di lanciare il server web e possiamo farlo in questo modo:
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Ciao a tutti, sono un web server!');
 });
+//Tutte queste 4 righe corrispondono ad una singola chiamata del metodo createServer()!
+// La funzione di callback che gli stiamo passando verrà richiamata nel momento in cui un visitatore si connetterà al sito.
 server.listen(8080);
