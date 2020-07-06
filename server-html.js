56214ef77117a32bb00e66daea0634fa33b96c4a
Choppychan <s.carmen2101@gmail.com>
2020-07-06 20:31:53 +0200

    Creato server-html

---
+++ server-html.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write('Hey!');
+    res.end();
+});
+
+server.listen(8080);
