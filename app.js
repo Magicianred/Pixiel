3c894e2c08bde0ab5a82089c68dfba4d49bc944c
Choppychan <s.carmen2101@gmail.com>
2020-07-08 18:49:03 +0200

    Creare routing di base

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('la richiesta è stata fatta da: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    if(req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/robe'){
+        var robe = [{name: 'carmelo', age: 30}, {name: 'giuseppina', age: 80}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(robe));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 server.listen(3000, '127.0.0.1');
