0cf06dd319899e0e99ee71a1a4d0d909f3a4857d
Choppychan <s.carmen2101@gmail.com>
2020-07-08 20:25:52 +0200

    Query Strings Tutorial

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ app.set('view engine', 'ejs');
 //Serve per linkare di directory css, il primo determina il route, il secondo la cartella utilizzata
 //Infatti se scriverò l'url cosi: http://127.0.0.1:3000/cose/index.css mi apparirà tutto ciò che ho scritto nel file index.css
 //Dunque ci permetterà di utilizzare direttamente il file e di evitare lo <style></style>
-app.use('/cose', express.static('css'));
+//RICORDA! in questo caso devono essere uguali, che sennò non viene applicato il css alle pagine
+app.use('/css', express.static('css'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact', {qs: req.query});
 });
 
 app.get('/profile/:name', function(req, res) {
