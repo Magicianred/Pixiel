d8cd60e5f24205895cf063a0ee5c859a5def606d
Choppychan <s.carmen2101@gmail.com>
2020-07-08 18:31:38 +0200

    Ho seguito la guita che parla di come creare una pagina HTML

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('la richiesta è stata fatta da: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey clochard!');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
