316ff04af7e1603284fc3d0495b639833af6a001
Choppychan <s.carmen2101@gmail.com>
2020-07-08 19:35:21 +0200

    Utilizzate le pagine html e aggiunte delle variabili, con alcuni parametri, come da tutorial

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-res.send('Questa è l\'homepage');
+res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
-    res.send('Questa è la pagina contatti');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function(req, res) {
-res.send('Questo è il profilo di ' + req.params.name);
+    var data = {age: 25, job: 'ballerina'};
+res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
