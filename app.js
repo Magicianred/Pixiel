ab04a18737b427257b4cdcd6768de9f7052561c6
Choppychan <s.carmen2101@gmail.com>
2020-07-08 20:05:32 +0200

    ci permette di utilizzare direttamente il file css, invece che usare lo <style></style> direttamente nella pagina html

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+//Serve per linkare di directory css, il primo determina il route, il secondo la cartella utilizzata
+//Infatti se scriverò l'url cosi: http://127.0.0.1:3000/cose/index.css mi apparirà tutto ciò che ho scritto nel file index.css
+//Dunque ci permetterà di utilizzare direttamente il file e di evitare lo <style></style>
+app.use('/cose', express.static('css'));
 
 app.get('/', function (req, res) {
-res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
@@ -13,7 +17,7 @@ app.get('/contact', function (req, res) {
 
 app.get('/profile/:name', function(req, res) {
     var data = {age: 25, job: 'ballerina', hobbies: ['mangiare', 'cantare', 'divertirsi']};
-res.render('profile', {person: req.params.name, data: data});
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
