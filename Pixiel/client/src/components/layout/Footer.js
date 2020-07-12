0c4d072a46e326a03f71888afa2acc66f89e6ba1
Choppychan <s.carmen2101@gmail.com>
2020-07-12 15:20:39 +0200

    Aggiunto Footer

---
+++ Pixiel/client/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TableFooter } from '@material-ui/core';
+
+function Footer() {
+    return (
+        <>
+            <TableFooter>
+                Da aggiungere
+            </TableFooter>
+        </>
+    )
+}
+
+export default Footer;
