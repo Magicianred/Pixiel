5893f25a1c663c540edb6affdf2354c5a518321f
Choppychan <s.carmen2101@gmail.com>
2020-07-16 23:01:57 +0200

    Ho provato a customizzare i font di default, ho messo solo il logo principale e un bottone per provare.

--- Pixiel/client/src/App.js
+++ Pixiel/client/src/App.js
@@ -4,19 +4,35 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Footer from "./components/layout/Footer";
 
+//Material UI
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 //Pagina CSS creata da me
 import "./index.css";
 
 const App = () => {
+    const theme = createMuiTheme(
+        {
+            typography: {
+                button: {
+                    fontFamily: "Nishuki Pixels"
+                },
+                h1: {
+                    fontFamily: "Monster Friend Fore",
+                },
+            }
+        });
 
     return (
         <Router>
 
+            <MuiThemeProvider theme={theme}>
             <Switch>
                 <Route path="/" exact component={Home} />
             </Switch>
 
             <Footer />
+            </MuiThemeProvider>
 
         </Router>
     );
--- Pixiel/client/src/components/pages/Home.js
+++ Pixiel/client/src/components/pages/Home.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Home() {
-return (
-        <h1>Pixiel</h1>
+//Material-UI
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { deepPurple } from '@material-ui/core/colors';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
 
-)
+
+//Colore e style del bottone per l'accesso
+const ColorButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText(deepPurple[500]),
+        fontSize: 20,
+        backgroundColor: deepPurple[500],
+        '&:hover': {
+            backgroundColor: deepPurple[300],
+        },
+    },
+}))(Button);
+
+function Home( props ) {
+    const { classes } = props;
+    return (
+        <>
+                <Container className={classes} fixed>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+
+                            <Typography variant="h1">
+                                Pixiel
+                            </Typography>
+                        <ColorButton variant="contained" color="primary" size="large">
+                            Accedi
+                        </ColorButton>
+                    </Grid>
+                </Container>
+        </>
+    )
 }
+
+Home.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 
 export default Home;
