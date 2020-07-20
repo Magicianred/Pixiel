c00ed5b2224ca57bd4e0ba51143cc249820a0626
Choppychan <s.carmen2101@gmail.com>
2020-07-20 19:28:30 +0200

    Sistemato il bottone per accedere alla stanza

--- Pixiel/client/src/components/pages/elements/home/JoinButton.js
+++ Pixiel/client/src/components/pages/elements/home/JoinButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
+//Material-UI
 import {withStyles} from "@material-ui/core/styles";
 import {deepPurple} from "@material-ui/core/colors";
 import Button from "@material-ui/core/Button";
 
-const JoinButton = () => {
+const JoinButton = ({ nickName, room }) => {
     //Colore e style del bottone per l'accesso
     const ColorButton = withStyles((theme) => ({
         root: {
@@ -13,17 +16,24 @@ const JoinButton = () => {
             '&:hover': {
                 backgroundColor: deepPurple[300],
             },
+            margin: theme.spacing(6, 0, 2),
         },
     }))(Button);
 
     return (
         <>
-            <ColorButton
-                variant="contained"
-                color="primary"
-                size="large">
-                Accedi
-            </ColorButton>
+            {/*Se il bottone viene cliccato senza aggiungere i due valori, cioè nickName e room, non viene eseguito l'evento
+            in caso contrario riporta alla pagina della chat, con il link che ha al suo interno i due dati immessi*/}
+            <Link style={{ textDecoration: 'none' }} onClick={event => (!nickName || !room) ? event.preventDefault() : null} to={`/chat?nickName=${nickName}&room=${room}`}>
+                <ColorButton
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    type="submit"
+                >
+                    Accedi
+                </ColorButton>
+            </Link>
         </>
     )
 }
