b12e75b3ad1364337e942abd796c491f51090934
Choppychan <s.carmen2101@gmail.com>
2020-07-20 19:27:48 +0200

    Aggiunte le due sezioni del form, devo solo sistemare come più mi aggrada

--- Pixiel/client/src/components/pages/elements/home/FormToJoin.js
+++ Pixiel/client/src/components/pages/elements/home/FormToJoin.js
@@ -3,13 +3,22 @@ import React from "react";
 //Material-UI
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from '@material-ui/core/styles';
+import {deepPurple} from "@material-ui/core/colors";
 
-const FormToJoin = () => {
+const FormToJoin = ({ setNickName, setRoom }) => {
+
     const useStyles = makeStyles((theme) => ({
         root: {
+            color: theme.palette.getContrastText(deepPurple[500]),
             '& > *': {
                 margin: theme.spacing(1),
                 width: '25ch',
+            },
+            paper: {
+                marginTop: theme.spacing(8),
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
             },
         },
     }));
@@ -18,10 +27,20 @@ const FormToJoin = () => {
 
         return (
             <>
-                <form className={classesFormToJoin.root} noValidate autoComplete="off">
+                <form className={classesFormToJoin.root, classesFormToJoin.paper} noValidate autoComplete="off">
                     <TextField
-                        id="standard-basic"
-                        label="Nickname"/>
+                        id="Nickname"
+                        label="Nickname"
+                        type="text"
+                        onChange={(event) => setNickName(event.target.value)}
+                    />
+
+                    <TextField
+                        id="Room"
+                        label="Stanza"
+                        type="text"
+                        onChange={(event) => setRoom(event.target.value)}
+                    />
                 </form>
             </>
         )
