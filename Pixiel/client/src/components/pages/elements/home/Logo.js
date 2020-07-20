d812f7c7d3f13be911d6bd991126d8ea86e0c9dd
Choppychan <s.carmen2101@gmail.com>
2020-07-20 18:22:39 +0200

    Creato componente Logo

---
+++ Pixiel/client/src/components/pages/elements/home/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const Logo = () => {
+    return(
+        <>
+            <Typography variant="h1">
+                Pixiel
+            </Typography>
+        </>
+    )
+}
+
+export default Logo.js
