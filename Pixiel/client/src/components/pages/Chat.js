37b2952be1d4859e218ed2aa6de1a3ff528501dc
Choppychan <s.carmen2101@gmail.com>
2020-07-22 19:54:26 +0200

    Collegamente tra il client e il server, che ci permette di vedere connessione e disconnessione di un utente, insieme al nome utilizzato per il nickName e la room

--- Pixiel/client/src/components/pages/Chat.js
+++ Pixiel/client/src/components/pages/Chat.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    //Hooks
+    const [nickName, setNickName] = useState('');
+    const [room, setRoom] = useState('');
+
+
+    const ENDPOINT = 'localhost:3001';
+
+
+    useEffect(() => {
+        const { nickName, room } = queryString.parse(window.location.search);
+
+        socket = io(ENDPOINT);
+        setNickName(nickName);
+        setRoom(room);
+
+        //Permette di visualizzare i dati immessi come nickName e room
+        socket.emit('join', { nickName, room }, ({error}) => {
+
+        });
+        //Parte di codice che riguarda la disconnessione di un utente
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
+
+
     return (
         <h1>Chat</h1>
     )
