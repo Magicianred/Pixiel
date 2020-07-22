754aeb7e38ef7d38e1467c0f34bfde1940b41a41
Choppychan <s.carmen2101@gmail.com>
2020-07-22 19:51:51 +0200

    Aggiunte, lato server, parte di codice che ci permettono di visualizzare dalla console quando un utente si connette, che nickname e che room ha immesso nei form e quando si disconnette

--- Pixiel/server/server.js
+++ Pixiel/server/server.js
@@ -1,6 +1,36 @@
 const express = require("express");
-const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Server in esecuzione nella porta ${PORT}`));
+//Utilizziamo il file nella cartella routes
+const  router = require("./routes/index");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+//Connessione al server
+io.on('connection', (socket) => {
+    console.log('Si è connesso qualcuno');
+
+    //Nickname e room utilizzate per la chat
+    socket.on('join', ({nickName, room}, callback) => {
+    console.log(nickName, room);
+
+    })
+
+    //Disconnessione dal server
+    socket.on('disconnect', () => {
+        console.log('Un utente si è disconnesso');
+    })
+});
+
+
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server in esecuzione nella porta ${PORT}`));
+
+
