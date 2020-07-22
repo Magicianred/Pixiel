0e63b635774ff66868119f6cb39bb28fd3b0dd90
Choppychan <s.carmen2101@gmail.com>
2020-07-23 00:39:29 +0200

    importati i moduli su server.js e aggiunte alcune funzioni da integrare successivamente nella parte client.

--- Pixiel/server/server.js
+++ Pixiel/server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require ("./users.js");
+
 const PORT = process.env.PORT || 3001;
 
 //Utilizziamo il file nella cartella routes
@@ -13,13 +15,30 @@ const io = socketIo(server);
 
 //Connessione al server
 io.on('connection', (socket) => {
-    console.log('Si è connesso qualcuno');
-
     //Nickname e room utilizzate per la chat
     socket.on('join', ({nickName, room}, callback) => {
-    console.log(nickName, room);
+        const { error, user } = addUser({ id: socket.id, nickName, room });
 
-    })
+        if(error) return callback(error);
+
+        //Messaggio che appare all'utente appena entrato
+        socket.emit('message', { user: 'admin', text: `${user.nickName}, benvenuto/a nella stanza ${user.room}`});
+        //Alle persone nella stanza appare un messaggio che permettere di sapere chi è entrato in quel momento
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.nickName} è entrato nella stanza!`});
+        socket.join(user.room);
+
+        callback();
+    });
+
+
+    socket.on('sentMessage', ( message, callback ) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.nickName, text: message});
+
+        callback();
+    });
+
 
     //Disconnessione dal server
     socket.on('disconnect', () => {
