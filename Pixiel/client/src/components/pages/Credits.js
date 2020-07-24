1df242029e6451a3cc4c0ca51224ea0b83010fa6
Choppychan <s.carmen2101@gmail.com>
2020-07-24 19:04:30 +0200

    Aggiunto un nuovo componente Crediti

---
+++ Pixiel/client/src/components/pages/Credits.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Material-UI
+import Container from '@material-ui/core/Container';
+
+const Credits = () => {
+    return (
+        <>
+            <Container fixed>
+
+            </Container>
+            </>
+    )
+}
+
+export default Credits;
