6190923185f515f20403989046b13e84253b270e
Choppychan <s.carmen2101@gmail.com>
2020-07-24 19:29:41 +0200

    Creata la navigation bar per la pagina della chat

---
+++ Pixiel/client/src/components/pages/elements/chat/InfoBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+//Material UI
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {deepPurple} from "@material-ui/core/colors";
+
+//Colore e style della App Bar
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        fontFamily: "Nishuki Pixels",
+        flexGrow: 1,
+    },
+    colorDefault: {
+        backgroundColor: deepPurple[500],
+    },
+}));
+
+const InfoBar = ({ room }) => {
+
+    const classes = useStyles();
+
+    return (
+        <>
+            <div className={classes.root}>
+                <AppBar className={classes.colorDefault} position="fixed" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title}>
+                            Stanza {room}
+                        </Typography>
+                        <a href="/"><img src="./images/closeIcon.png" alt="Esci dalla stanza" /></a>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        </>
+    )
+}
+
+export default InfoBar;
