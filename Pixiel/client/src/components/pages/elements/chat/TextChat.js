a5c9820d8670e00c05df09a6f4745245ea8c6ab9
Choppychan <s.carmen2101@gmail.com>
2020-07-25 14:18:18 +0200

    Sistemati alcuni elementi dell'imput e aggiunto il bottone per inviare, ma lo devo ancora posizionare

--- Pixiel/client/src/components/pages/elements/chat/TextChat.js
+++ Pixiel/client/src/components/pages/elements/chat/TextChat.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 //Material-UI
 import TextField from "@material-ui/core/TextField";
+import Container from '@material-ui/core/Container';
+import {deepPurple} from "@material-ui/core/colors";
+import {makeStyles} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+//Componenti creati da me
+import SendButton from "./SendButton";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: deepPurple[500],
+    },
+    text: {
+        spacing: theme.spacing(200),
+    }
+}));
+
 
 const TextChat = ({ message, setMessage, socket }) => {
+
+    const classes = useStyles();
 
     //Funzione per mandare i messaggi
     const sendMessage = (event) => {
@@ -16,12 +37,28 @@ const TextChat = ({ message, setMessage, socket }) => {
 
     return (
         <>
-            <TextField
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
-                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null }
-            />
-            </>
+            <Container fixed>
+            <Box alignItems="flex-end" display="flex">
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField
+                    className={classes.text}
+                    type="text"
+                    placeholder="Scrivi un messaggio..."
+                    label="Standard"
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null }
+                />
+
+                {/*Bottone per mandare i messaggi*/}
+                <SendButton
+                sendMessage={sendMessage}
+                />
+
+            </form>
+            </Box>
+            </Container>
+        </>
     )
 }
 
