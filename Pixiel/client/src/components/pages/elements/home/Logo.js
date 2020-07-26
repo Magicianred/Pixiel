fb7b01fb3bb4cdc12a75fddb5bb455a473727a2b
Choppychan <s.carmen2101@gmail.com>
2020-07-26 15:05:26 +0200

    Aggiunto il margine attorno al logo

--- Pixiel/client/src/components/pages/elements/home/Logo.js
+++ Pixiel/client/src/components/pages/elements/home/Logo.js
@@ -1,12 +1,17 @@
 import React from "react";
+
+//Material-UI
+import Box from '@material-ui/core/Box';
 import Typography from "@material-ui/core/Typography";
 
 const Logo = () => {
     return(
         <>
+            <Box m={3}>
             <Typography variant="h1">
                 Pixiel
             </Typography>
+            </Box>
         </>
     )
 }
