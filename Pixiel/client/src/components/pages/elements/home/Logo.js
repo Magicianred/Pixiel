37e12284964b1b4e0f0a7c511707025728b87c55
Choppychan <s.carmen2101@gmail.com>
2020-08-04 21:05:10 +0200

    Reso il logo responsive per schermi piccoli

--- Pixiel/client/src/components/pages/elements/home/Logo.js
+++ Pixiel/client/src/components/pages/elements/home/Logo.js
@@ -3,12 +3,27 @@ import React from "react";
 //Material-UI
 import Box from '@material-ui/core/Box';
 import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        fontFamily: "Monster Friend Fore",
+        [theme.breakpoints.only('xs')]: {
+            fontSize: 54,
+        },
+    }
+}));
+
 
 const Logo = () => {
+
+    const classes = useStyles();
+
     return(
         <>
             <Box m={3}>
-            <Typography variant="h1">
+            <Typography className={classes.root} variant="h1">
                 Pixiel
             </Typography>
             </Box>
