b5ff2820e1cd06d49ca8e29e2680a3295d3bcf2f
Choppychan <s.carmen2101@gmail.com>
2020-08-04 21:12:17 +0200

    Aggiunti alcuni crediti

--- Pixiel/client/src/components/pages/Credits.js
+++ Pixiel/client/src/components/pages/Credits.js
@@ -7,9 +7,11 @@ const Credits = () => {
     return (
         <>
             <Container fixed>
-
+                Sito realizzato da <a href="https://www.carmensantoro.it">Santoro Carmen Agnese</a><br />
+                Font Dogica Pixel di <a href="https://www.patreon.com/rmocci">Roberto Mocci</a><br />
+                Font Monster Friend Fore di <a href="https://www.dafont.com/monster-friend.d8363">Monster Friend</a><br />
             </Container>
-            </>
+        </>
     )
 }
 
