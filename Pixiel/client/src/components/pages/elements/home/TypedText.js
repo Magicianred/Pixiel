b5e37040d09e6c6b3f1565f2b0b782d460fa65d7
Choppychan <s.carmen2101@gmail.com>
2020-08-05 11:06:01 +0200

    Sistemato il testo e settato per schermi più piccoli

--- Pixiel/client/src/components/pages/elements/home/TypedText.js
+++ Pixiel/client/src/components/pages/elements/home/TypedText.js
@@ -1,20 +1,36 @@
 import React from "react";
 import Typed from "react-typed";
+import {makeStyles} from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        fontSize: 18,
+        fontFamily: "Dogica Pixel",
+        [theme.breakpoints.only('xs')]: {
+            fontSize: 10,
+        },
+    }
+}));
+
 
 //Testo animato della pagina principale
 const textLines = [
-    `Benvenuto su Pixiel`,
-    `puoi creare la tua chat room`,
-    `e invitare chi vuoi`,
+    `Benvenuto su Pixiel!`,
+    `Qui puoi creare la tua stanza`,
+    `e chattare con chi vuoi`,
     `Immetti il tuo Nickname`,
-    `e il nome della stanza`,
+    `e il nome della Stanza`,
     `per chattare con i tuoi amici!`
 ];
 
 const TypedText = () => {
+
+    const classes = useStyles();
+
         return (
             <>
-                <div style={{ fontSize: "20px", fontFamily: "Dogica Pixel" }}>
+                <div className={classes.root}>
                 <Typed strings={textLines} typeSpeed={100} />
                 </div>
             </>
