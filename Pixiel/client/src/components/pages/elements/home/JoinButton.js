e8256ff74a942f623ffc112b8239a43848011c9e
Choppychan <s.carmen2101@gmail.com>
2020-08-05 12:53:50 +0200

    Fixato, visto che mi dava un errore nel codice

--- Pixiel/client/src/components/pages/elements/home/JoinButton.js
+++ Pixiel/client/src/components/pages/elements/home/JoinButton.js
@@ -38,20 +38,21 @@ const JoinButton = ({ nickName, room, setOpen }) => {
     return (
         <>
             <Grid
+                container
                 alignItems="center"
-                  justify="center"
+                justify="center"
             >
-            {/*Se il bottone viene cliccato senza aggiungere i due valori, cioè nickName e room, non viene eseguito l'evento
+                {/*Se il bottone viene cliccato senza aggiungere i due valori, cioè nickName e room, non viene eseguito l'evento
             in caso contrario riporta alla pagina della chat, con il link che ha al suo interno i due dati immessi*/}
-            <Link style={{ textDecoration: 'none' }} onClick={handleClick} to={`/chat?nickName=${nickName}&room=${room}`}>
-                <ColorButton
-                    variant="contained"
-                    size="large"
-                    type="submit"
-                >
-                    Accedi
-                </ColorButton>
-            </Link>
+                <Link style={{ textDecoration: 'none' }} onClick={handleClick} to={`/chat?nickName=${nickName}&room=${room}`}>
+                    <ColorButton
+                        variant="contained"
+                        size="large"
+                        type="submit"
+                    >
+                        Accedi
+                    </ColorButton>
+                </Link>
             </Grid>
         </>
     )
