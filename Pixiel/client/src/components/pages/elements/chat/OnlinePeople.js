3b6a1a718304cceb05297f8c90f68b2939eebb20
Choppychan <s.carmen2101@gmail.com>
2020-08-07 15:28:11 +0200

    Resa responsive la barra con le persone online

--- Pixiel/client/src/components/pages/elements/chat/OnlinePeople.js
+++ Pixiel/client/src/components/pages/elements/chat/OnlinePeople.js
@@ -2,25 +2,58 @@ import React from "react";
 
 //Material-UI
 import Box from "@material-ui/core/Box";
+import {makeStyles} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: theme.palette.primary.contrastText,
+        fontFamily: "Dogica Pixel",
+        fontSize: 10,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 7,
+        },
+    },
+    onlinePeople: {
+        padding: 20,
+        [theme.breakpoints.down('sm')]: {
+            padding: 10,
+        },
+    },
+    icon: {
+      paddingRight: 5,
+        height: 14,
+        [theme.breakpoints.down('sm')]: {
+            height: 10,
+        },
+    },
+    h1: {
+        display: 'inline',
+    }
+}));
 
 
 const OnlinePeople = ({ users }) => {
+
+    const classes = useStyles();
+
     return (
         <>
             {
                 users ? (
                     <>
-                        <h1>Persone attualmente online:</h1>
-                        <Box>
-                            <h2>
+                        <Grid direction="row" className={classes.root}>
+                        <h1 className={classes.h1}>Persone online:</h1>
+                            <h2 className={classes.h1}>
                                 {users.map(({nickName}) => (
-                                    <div key={nickName} className="activeItem">
+                                    <span key={nickName} className={classes.onlinePeople}>
+                                        <img className={classes.icon} alt="persona online" src="./images/people.png" />
                                         {nickName}
-                                        <img alt="persona online" src="./images/people.png" />
-                                    </div>
+                                    </span>
                                 ))}
                             </h2>
-                        </Box>
+                        </Grid>
                         </>
                 )
                     : null
